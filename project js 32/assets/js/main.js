44c1503189b72fca53628dc44f174571ed620967
buian90 <cungbuian1990@gmail.com>
2023-02-20 23:26:32 +0700

    bài tập 11

--- project js 32/assets/js/main.js
+++ project js 32/assets/js/main.js
@@ -1,41 +1,74 @@
-// Bài 2.1 :
-function checkMark(mark) {
-    if (mark >= 85) { console.log ("A");
-        
-    } else if ( 70 <= mark && mark < 85){ console.log ("B"); }
-        else if ( 40 <= mark && mark < 70){console.log("C");}
-        else {console.log("D");}
-    }
-// Bài 2.2
-    function checkMax(a,b) {
-        if (a > b) { console.log("a");
-            
-        } else { console.log("b");
-            
-        }
-        
-    }
-    // Bài 2.3
-function checkNumber(a) {
-    if (a > 0) {console.log("a là số dương");}
-        else if ( a < 0) {console.log("a là số âm");}
-        else {console.log("a là số 0");}
-    }
-    // Bài 2.4
-    function checkNumber1(a) {
-        if ( a % 2 == 0 ){console.log("a là số chẵn");}
-        else {console.log("a là số lẻ");} 
-    }
-    // Bài 2.5
-function checkNumber2(a) {
-    if (a % 3 == 0 && a % 5 == 0) {console.log("a chia hết cho 3 và 5");}
-    else {console.log("a ko chia hết cho 3 và 5");}
-}
-// Bài 2.6
-function checkNumber3(a,b,c) {
-    if (a + b === c) { console.log("c = a + b");
-    }
-}
+
+//  Bài tập 1 
+ function chuoiString(param) {
+      let ketQua = []
+      for (let index = 0; index < 10; index++) {
+     ketQua += param 
+     }
+     console.log(ketQua);
+  } chuoiString ("A")
+
+//  Bài tập 2
+  function repeatString(param) {
+      let ketQua = []
+      for (let index = 0; index < 10; index++) {
+           ketQua.push(param); 
+      }
+      let text = ketQua.join("-");
+   console.log(text);
+   }repeatString ("A")
+
+// Bài tập 3
+ function chuoiString(chu, so) {
+     let arr = [];
+      for (let index = 0; index < so; index++) {
+        arr.push(chu);
+       }
+    let text = arr.join("-");
+       console.log(text);
+      }
+      chuoiString("a", 5);
+
+//  Bài tập 4 
+ function checkNumb() {
+     let sum = 0
+     let arr = []
+     for (let index = 0; index <= 100; index++) {
+     if (index % 5 == 0 && index !== 0) {
+         sum += index
+         arr.push(index)
+     }
+     }
+     console.log(arr.join(" "));
+     console.log(sum);
+ } checkNumb ()
+
+//  Bài tập 5
+ function theTich(r) {
+ let v = 0;
+ v = (4 * 3.14 * r ** 3) / 3
+ return v;    
+ }
+
+//  bài Tập 6
+
+ function tinhTong(a, b) {
+      
+     let sum = 0;
+      
+     if (a > b) {
+       for (let index = a - 1; index > b; index--) {
+         sum += index;
+       }
+     } else {
+       for (let index = a + 1; index < b; index++) {
+         sum += index;
+       }
+     }
+     return sum;
+   }
+
+    // bài tập 7
 
 
 
