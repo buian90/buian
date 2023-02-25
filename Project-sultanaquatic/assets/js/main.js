7b726067001051cdab9423ec554620cd1b4feb59
buian90 <cungbuian1990@gmail.com>
2023-02-26 02:00:19 +0700

    update sultan web

---
+++ Project-sultanaquatic/assets/js/main.js
@@ -0,0 +1,53 @@
+
+// slider auto next
+var slider = {
+    currentSlide: 0,
+    slides: document.querySelectorAll('.slide'),
+  
+    showSlide: function(n) {
+      this.slides[this.currentSlide].style.display = 'none';
+      this.currentSlide = (n + this.slides.length) % this.slides.length;
+      this.slides[this.currentSlide].style.display = 'block';
+    },
+  
+    nextSlide: function() {
+      this.showSlide(this.currentSlide + 1);
+    },
+  
+    prevSlide: function() {
+      this.showSlide(this.currentSlide - 1);
+    },
+  
+    startSlideShow: function() {
+      setInterval(() => {
+        this.nextSlide();
+      }, 2000);
+    }
+  };
+  
+  slider.showSlide(slider.currentSlide);
+  slider.startSlideShow();
+
+// best seller hover thay đổi ảnh
+
+const products = document.querySelectorAll('.product');
+
+products.forEach((product) => {
+  const img = product.querySelector('img');
+  const originalSrc = img.src;
+  const hoverSrc = img.getAttribute('data-hover-src');
+
+  img.addEventListener('mouseover', () => {
+    img.src = hoverSrc;
+  });
+
+  img.addEventListener('mouseout', () => {
+    img.src = originalSrc;
+  });
+});
+
+
+
+  
+
+  
