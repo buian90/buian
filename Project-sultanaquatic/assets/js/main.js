8228be3f1e5e647db1894d328850852a24eccecb
buian90 <cungbuian1990@gmail.com>
2023-02-26 23:38:01 +0700

    update

--- Project-sultanaquatic/assets/js/main.js
+++ Project-sultanaquatic/assets/js/main.js
@@ -1,53 +1,52 @@
 
 // slider auto next
 var slider = {
-    currentSlide: 0,
-    slides: document.querySelectorAll('.slide'),
-  
-    showSlide: function(n) {
-      this.slides[this.currentSlide].style.display = 'none';
-      this.currentSlide = (n + this.slides.length) % this.slides.length;
-      this.slides[this.currentSlide].style.display = 'block';
-    },
-  
-    nextSlide: function() {
-      this.showSlide(this.currentSlide + 1);
-    },
-  
-    prevSlide: function() {
-      this.showSlide(this.currentSlide - 1);
-    },
-  
-    startSlideShow: function() {
-      setInterval(() => {
-        this.nextSlide();
-      }, 1500);
-    }
-  };
-  
-  slider.showSlide(slider.currentSlide);
-  slider.startSlideShow();
+  currentSlide: 0,
+  slides: document.querySelectorAll('.slide'),
+
+  showSlide: function(n) {
+    this.slides[this.currentSlide].style.display = 'none';
+    this.currentSlide = (n + this.slides.length) % this.slides.length;
+    this.slides[this.currentSlide].style.display = 'block';
+  },
+
+  nextSlide: function() {
+    this.showSlide(this.currentSlide + 1);
+  },
+
+  prevSlide: function() {
+    this.showSlide(this.currentSlide - 1);
+  },
+
+  startSlideShow: function() {
+    setInterval(() => {
+      this.nextSlide();
+    }, 1500);
+  }
+};
+
+slider.showSlide(slider.currentSlide);
+slider.startSlideShow();
 
 // best seller hover thay đổi ảnh
 
 const products = document.querySelectorAll('.product');
 
 products.forEach((product) => {
-  const img = product.querySelector('img');
-  const originalSrc = img.src;
-  const hoverSrc = img.getAttribute('data-hover-src');
+const img = product.querySelector('img');
+const originalSrc = img.src;
+const hoverSrc = img.getAttribute('data-hover-src');
 
-  img.addEventListener('mouseover', () => {
-    img.src = hoverSrc;
-  });
+img.addEventListener('mouseover', () => {
+  img.src = hoverSrc;
+});
 
-  img.addEventListener('mouseout', () => {
-    img.src = originalSrc;
-  });
+img.addEventListener('mouseout', () => {
+  img.src = originalSrc;
+});
 });
 
 
 
-  
 
-  
+
