4006a2ca74d08f6fd6955d4d63af9c686eef56c0
buian90 <cungbuian1990@gmail.com>
2023-03-14 18:57:50 +0700

    update

--- Movie-app/assets/js/listing.js
+++ Movie-app/assets/js/listing.js
@@ -1,9 +1,34 @@
-//  buoc 1 khai bao bien
-const API_URL_LISTING = "https://api-online-seven.vercel.app/products";
-// buoc 2 goi du lieu
-async function getListingAPI(api_url){
-  const res = await.axios.get(`${api_url}`);
-  const data = await res.data;
-  showListing(data);
-} 
-// buoc 3 hien thi du lieu
+/*Buoc 1: Khai bao bien*/
+const API_URL_LISING = "https://api-online-seven.vercel.app/products"
+const URL = "https://api-online-seven.vercel.app";
+/*Buoc 2: Goi du lieu*/
+getListingAPI(API_URL_LISING);
+
+async function getListingAPI(api_url) {
+    const res = await axios.get(`${api_url}`);
+    const data = await res.data;
+    // console.log(data);
+    showListing(data);
+}
+
+/*----Buoc 3: Hien thi du lieu---*/
+
+
+function showListing(data){
+    let htmlCode = ``;
+    data.forEach(function(value){
+        htmlCode += `
+        <div class="col-12 col-sm-6 col-md-3">
+            <a href="/detail.html?id=${value.id}">
+                <img src="${URL + value.image}" />
+            </a>
+        </div>
+        `;
+
+        // Truy cap phan tu
+        const content = document.querySelector('.listing-ecommerce .row');
+        // console.log(content);
+        content.innerHTML = htmlCode;
+    })
+
+}
